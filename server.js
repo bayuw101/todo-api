9002c7d7efccf34581852b3de1a282c1e1506a43
Bayu <emailbayuwicaksono@gmail.com>
2017-03-01 11:13:35 +0700

    add delete

--- server.js
+++ server.js
@@ -56,15 +56,20 @@ app.get('/todos/:id', function(req, res) {
 
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id);
-	var matchTodo = _.findWhere(todos, {
-		id: todoId
+	
+	db.todo.destroy({
+		where : {
+			id : todoId
+		}
+	}).then(function(rowsDeleted){
+		if(rowsDeleted <= 0){
+			res.status(404).json({error:"No Data Deleted !"});
+		}else{
+			res.status(204).send();
+		}
+	}).catch(function(e){
+		res.status(500).send();
 	});
-	if (!matchTodo) {
-		res.send(404).json([]);
-	} else {
-		todos = _.without(todos, matchTodo);
-		res.json(todos);
-	}
 });
 
 app.post('/todos', function(req, res) {
